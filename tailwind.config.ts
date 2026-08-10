b5e044d200522021594d76b5c39c3c2a847c923f
apptaok-wq <apptaok@gmail.com>
2026-08-10 11:28:41 +0700

    Create tailwind.config.ts with correct content path

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './src/**/*.{ts,tsx,js,jsx,html}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
